557ba92aa932031fd8a6c4454bbc5e1dfd6ae554
Antonio de Sousa <antoniojsba@gmail.com>
2021-03-10 10:34:52 +0100

    Limit range of dates until today

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
+import moment from 'moment';
 import { connect, DispatchProp } from 'react-redux';
 import { initiateGetCommits } from '../services/resources';
 import { TCommit } from '../utils/interfaces';
 import { ListCommits } from './ListCommits';
+import { RangePicker } from '../library/date-picker/DatePicker';
+import { Button } from '../library/button/Button'
 
 export interface IOverview extends DispatchProp<any> {
 	commits: TCommit[];
@@ -18,6 +21,10 @@ const Overview = (props: IOverview): JSX.Element => {
 	return (
 		<div className="overview">
 			<h2>List of commits:</h2>
+			<div className={'date-range'}>
+				<RangePicker disabledDate={(current) => current && current > moment().endOf('day')}/>
+				<Button type={'primary'}>Search</Button>
+			</div>
 			{commits && <ListCommits source={commits}/>}
 		</div>
 	);
